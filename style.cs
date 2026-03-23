227972766592b8b60dee08d4acea7c0525b775a2
bobinbabu2004 <Bobinbabu54@gmail.com>
2026-03-24 00:35:27 +0530

    trending page aadeed

---
+++ style.cs
@@ -0,0 +1,11 @@
+
+.card-hover {
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  cursor: pointer;
+}
+
+.card-hover:hover {
+  transform: scale(1.08);
+  z-index: 5;
+  box-shadow: 0 10px 25px rgba(0,0,0,0.7);
+}
